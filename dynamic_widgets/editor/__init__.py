fdb9fb239dd05b711de8c3a17996ccc1b5f8fe81
Ludwig Kraatz <ludi182@hotmail.com>
2013-09-24 09:30:32 +0200

    hard coded editor template within getter method (circular import issues)

--- dynamic_widgets/editor/__init__.py
+++ dynamic_widgets/editor/__init__.py
@@ -1,56 +1,58 @@
-from dynamic_widgets import get_widget_model, get_django_template_model
-Widget = get_widget_model()
-DjangoTemplate = get_django_template_model()
-
-editor_widget = Widget(
-    identifier      = 'widget-editor',
+def get_editor_template():
+    from dynamic_widgets import get_widget_model, get_django_template_model
+    Widget = get_widget_model()
+    DjangoTemplate = get_django_template_model()
     
-)
-editor_template = DjangoTemplate(
-    widget      = editor_widget,
-    content     = """<div id='status'></div>
-                Select Widget<select id='WidgetSelect' ></select> <input type='button' value='new'  id='add_new_widget'/><br />
-                <div class="widget-editor-menu">
-                  <ul>
-                    <li><a href="#template-editor">Template Editor</a></li>
-                    <li><a href="#dynamic_content">Dynamic Content</a></li>
-                    <li><a href="#wordings">Wordings</a></li>
-                    <li><a href="#preview">Preview</a></li>
-                    <li><a href="#widget-settings">Settings</a></li>
-                  </ul>
-                  <div id='dynamic_content' class="dynamic-widget" data-widget-name="dynamic-content-editor">
-                    <div class="dynamic-content-container"></div>
-                  </div>
-                  <div id='preview' class="dynamic-widget" data-widget-name="dynamic-template-preview"></div>
-                  <div id="widget-settings">
-                     <div id='widget_settings' style='display:none;'>
-                         <input type='checkbox' id='is_widget_staff'><label for='is_widget_staff'>For Staff</label><br />
-                         <input type='checkbox' id='is_widget_public'><label for='is_widget_public'>Public</label><br />
-                    </div>
-                  </div>
-                  <div id='template-editor' class="dynamic-widget" data-widget-name="dynamic-template-editor">
-                     <form>
-                        <textarea name="content" id='template_editor_window' class="editor_window" style='height:450px;width:100%'></textarea>
-                        <div style='float:left;'>
-                            <input type='text' value='' name='identifier' class='template_identifier'/>
+    editor_widget = Widget(
+        identifier      = 'widget-editor',
+        
+    )
+    editor_template = DjangoTemplate(
+        widget      = editor_widget,
+        content     = """<div id='status'></div>
+                    Select Widget<select id='WidgetSelect' ></select> <input type='button' value='new'  id='add_new_widget'/><br />
+                    <div class="widget-editor-menu">
+                      <ul>
+                        <li><a href="#template-editor">Template Editor</a></li>
+                        <li><a href="#dynamic_content">Dynamic Content</a></li>
+                        <li><a href="#wordings">Wordings</a></li>
+                        <li><a href="#preview">Preview</a></li>
+                        <li><a href="#widget-settings">Settings</a></li>
+                      </ul>
+                      <div id='dynamic_content' class="dynamic-widget" data-widget-name="dynamic-content-editor">
+                        <div class="dynamic-content-container"></div>
+                      </div>
+                      <div id='preview' class="dynamic-widget" data-widget-name="dynamic-template-preview"></div>
+                      <div id="widget-settings">
+                         <div id='widget_settings' style='display:none;'>
+                             <input type='checkbox' id='is_widget_staff'><label for='is_widget_staff'>For Staff</label><br />
+                             <input type='checkbox' id='is_widget_public'><label for='is_widget_public'>Public</label><br />
                         </div>
-                        <div style='float:right;'>
-                            <input type='button' value='release' class='release_editor_content' style='display:none;'/>
-                            <input type='submit' value='save (CTRL+S)' class='save_editor_content'/>
-                        </div><div style='display: block;font-size: 0;line-height: 0;height: 0;clear: both;'>&nbsp;</div>
-                     </form>
-                  </div>
-                  <div id="wordings">
-                     <div class="dynamic-widget" data-widget-name="dynamic-wording-editor">
-                        <div class="list dynamic-list">
-                            <div class="dynamic-header">
-                                <div class="dynamic-list-entry-header" width=100>identifier</div>
+                      </div>
+                      <div id='template-editor' class="dynamic-widget" data-widget-name="dynamic-template-editor">
+                         <form>
+                            <textarea name="content" id='template_editor_window' class="editor_window" style='height:450px;width:100%'></textarea>
+                            <div style='float:left;'>
+                                <input type='text' value='' name='identifier' class='template_identifier'/>
                             </div>
-                            <div class="dynamic-list-container"></div>
-                        </div>
-                     </div>
-                     <input type="button" value="release" class="release_wordings">
-                  </div>
-                </div>""",
-    
-)
+                            <div style='float:right;'>
+                                <input type='button' value='release' class='release_editor_content' style='display:none;'/>
+                                <input type='submit' value='save (CTRL+S)' class='save_editor_content'/>
+                            </div><div style='display: block;font-size: 0;line-height: 0;height: 0;clear: both;'>&nbsp;</div>
+                         </form>
+                      </div>
+                      <div id="wordings">
+                         <div class="dynamic-widget" data-widget-name="dynamic-wording-editor">
+                            <div class="list dynamic-list">
+                                <div class="dynamic-header">
+                                    <div class="dynamic-list-entry-header" width=100>identifier</div>
+                                </div>
+                                <div class="dynamic-list-container"></div>
+                            </div>
+                         </div>
+                         <input type="button" value="release" class="release_wordings">
+                      </div>
+                    </div>""",
+        
+    )
+    return editor_template
